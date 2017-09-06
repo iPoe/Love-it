11ea394fa4f7f9c98f38978a839b381c742c1fa7
iPoe <leonardosawi12@gmail.com>
2017-09-06 16:51:29 -0500

    4
    Punto 3 no se puede divide y venceras

--- loveit1.cpp
+++ loveit1.cpp
@@ -18,39 +18,26 @@ char* split(char* cadena, int ini,int fin){
 }
 
 //Function that returns the amount of 0s of a chain
-
-int cantidad(char* cadena){
+int cantidad(char* cadena, int ini, int fini ){
 
     //We got the size of the char
-    int n= strlen(cadena);
-
-    if(n==1){
-        if(cadena[0]=='0'){
-
+    if(fini==ini ){
+        if(cadena[ini]=='0'){
             return 1;
         }
         else{
             return 0;
         }
     }
-
-
-
     else{
-        char* res = split(cadena,n/2,n);
-        char* res1 = split(cadena,0,n/2);
-
-        return cantidad(res)+cantidad(res1);
-
+        int mitad= ini+(fini-ini)/2;
+        char res = cantidad( cadena, ini, mitad);
+        char res1 = cantidad( cadena, mitad+1, fini );
+        return res+res1;
     }
-
 }
 
-
-
-
 //Brute force
-
 int amount(char chain[]){
     int n= strlen(chain), a=0;
 
@@ -66,12 +53,34 @@ int amount(char chain[]){
 }
 
 int main(){
-
+int a = 0;
+int b = 6;
     char x[]="100000";
+    char d[]="holaalaska";
     //printf("%i",amount(x));
-    printf("%i",cantidad(x));
+    //printf("%i",cantidad(x));
+    //printf("%i",cantidad(x,0,6));
+    printf("%i",islas(d));
 
 }
+
+int islas(char* chain){
+    int i=0;
+    int n = strlen(cadena);
+    if(n==1){
+        if(res[0]==res1[0]){
+            return 1;
+        }
+        else{
+            i+=1;
+        }
+    }
+    else{
+        char* res= split(chain,0,n/2);
+        char* res1=split(chain,n/2,n);
+    }
+
+ }
 
 
 
--- loveit2.cpp
+++ loveit2.cpp
@@ -3,39 +3,39 @@
 #include <stdio.h>
 #include <string.h>
 using namespace std;
+int main(){
+        int hola[]={1,-1,2,-2,-3};
+        cout<<negative(hola,5);
+ }
 
 //Develop split function
 int* split(int* cadena, int ini,int fin){
-        int tamano=fin+ini;
-        int* res=(int*)malloc(tamano);
-        for(int i=0; i<tamano;i++){
-            res[i]=cadena[i+ini];
-        }
+            int tamano=fin+ini;
+            int* res=(int*)malloc(tamano);
+            for(int i=0; i<tamano;i++){
+                res[i]=cadena[i+ini];
+            }
 
-        return res;
+            return res;
         }
  int negative(int* arreglo,int n){
 
+         if(n==1){
+            if(arreglo[0]<0){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+         }
 
-
-     if(n==1){
-        if(arreglo[0]<0){
-            return 1;
-        }
-        else{
-            return 0;
-        }
-     }
-
-     else{
-        int* arra=split(arreglo,0,n/2);
-        int* arra1=split(arreglo,n/2,n);
-        int x=n-n/2;
-        return negative(arra,n/2)+negative(arra1,x);
-     }
+         else{
+            int* arra=split(arreglo,0,n/2);
+            int* arra1=split(arreglo,n/2,n);
+            int x=n-n/2;
+            return negative(arra,n/2)+negative(arra1,x);
+         }
 
  }
- int main(){
-     int hola[]={1,-1,2,-2,-3};
-    cout<<negative(hola,5);
- }
+
+
