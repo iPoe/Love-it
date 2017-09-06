87799939a23ea9007ebd5649cec74be22e4643c1
iPoe <leonardosawi12@gmail.com>
2017-09-06 09:41:45 -0500

    1

---
+++ loveit1.cpp
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+//Function that returns the amount of 0s of a chain
+
+/*int cantidad(char cadena){
+
+    //We got the size of the char
+    int n= strlen(cadena);
+
+    int counter= 0;//This variable contains the amount of 0s
+    if(cadena[0]==0)
+        counter+=1;
+
+    else
+        cantidad(cadena)
+
+}
+
+        */
+//Brute force
+
+int amount(char chain){
+    int n= strlen(cadena), a=0;
+
+    for(int i=1;i<n;i++){
+        if(cadena[i]==o)
+            a+=1;
+        else
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
